766d5c6e8e62b26a4937a8177eac91b82ae24fde
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-06-10 23:23:58 +0100

    added request loop for request data until the user immit the right data

--- run.py
+++ run.py
@@ -21,15 +21,21 @@ def get_sales_data():
     """
     Get sales figures from user
     """
-    print("Please enter sales data from last market")
-    print("Data should be six numbers, separated by commas")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from last market")
+        print("Data should be six numbers, separated by commas")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str  = input("Enter your data here: ")
-    # print(f"The data provided is {data_str}")
+        data_str  = input("Enter your data here: ")
+        # print(f"The data provided is {data_str}")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if (validate_data(sales_data)):
+            print("Data is valid!")
+            break
+
+    
 
 def validate_data(values):
     """
@@ -43,7 +49,10 @@ def validate_data(values):
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
         print(f"invalid data {e}, please try again\n")
+        return False
+    
+    return True
 
-get_sales_data()
+data = get_sales_data()
 
 
